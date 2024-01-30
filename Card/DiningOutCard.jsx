b8a0c202ed4bc749a7f08951b49ba5b6056098c4
Debnath Mahapatra <128223356+Debnath23@users.noreply.github.com>
2024-01-30 21:22:12 +0530

    Add files via upload

---
+++ Card/DiningOutCard.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { CiStar } from "react-icons/ci";
+
+const DiningOutCard = ({data})=> {  
+  const {image, title, description, rating, forTwo, distance} = data;
+  return (
+    <div className="mt-5">
+      <div className="rounded-lg  hover:shadow-lg hover:border-solid hover:border-2 hover:border-slate-100 cursor-pointer  max-w-sm mx-auto bg-auto p-1 overflow-hidden">
+        <div className="">
+          <img
+            className="rounded-lg object-cover h-[250px] w-full"
+            src={image}
+            alt="image"
+          />
+        </div>
+        <div className="p-1 mt-1">
+          <div>
+            <div className="flex my-1 justify-between text-2xl text-black">
+              <div className="flex">
+                <p>{title}</p>
+              </div>
+              <div className="flex justify-end">
+                <p className="px-[2px] flex items-center justify-center bg-green-800 rounded-md text-white">{rating} <CiStar className="w-[18px] h-[18px]" /></p>
+              </div>
+            </div>
+
+            <div className="flex my-[2px] justify-between text-lg text-slate-500">
+              <div className="truncate">
+                <p>{description}</p>
+              </div>
+              <div className="flex items-end">
+                <p className="text-md flex">₹{forTwo} for two</p>
+              </div>
+            </div>
+
+            <div className="flex justify-end mt-[2px] text-md">
+             <p>{distance} km</p> 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> 
+  );
+}
+
+export default DiningOutCard;
+
